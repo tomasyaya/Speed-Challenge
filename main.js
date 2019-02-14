73b705a3a0c7cbfa35478611127cb2a4df6529c9
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-02-14 12:09:38 +0100

    main con pantallas integradas

--- main.js
+++ main.js
@@ -9,11 +9,52 @@ const main = () => {
     return main;
   };
 
-  
+  const buildSplashScreen = () => {
+    const splashScreen = buildDom(`
+      <section class="splash-screen">
+        <h1>Speed Challenge</h1>
+        <button class="start-game-button">Start!</button>
+      </section>
+    `)
+
+    const startButton = document.querySelector('.start-game-button');
+    startButton.addEventListener('click', buildGameScreen);
+  };
+
+  const buildGameScreen = () =>{
+    const gameScreen = buildDom(`
+      <section class="game-screen">
+      <canvas></canvas>
+      </screen>
+    `);
+
+    const width = document.querySelector('.game-screen').offsetWidth;
+    const height = document.querySelector('.game-screen').offsetHeight;
+
+    const canvasElement = document.querySelector('canvas');
+    canvasElement.setAttribute('width', width);
+    canvasElement.setAttribute('height', height);
+
+  };
+
+
+  const buildGameOverScreen = () => {
+    const gameOverScreen = buildDom(`
+      <section class="game-over-screen">
+        <h1>Game Over</h1>
+        <button class="restart-button">Restart</button>
+      </section>
+    `);
+
+    const restartButton = document.querySelector('.restart-button');
+    restartButton.addEventListener('click', buildGameScreen);
+
+  };
 
 
 
 
-}
+  buildSplashScreen();
+};
 
 window.addEventListener('load', main);
