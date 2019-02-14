672f15a26abd3d11017aebfbf206c0ec40355214
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2019-02-14 11:40:46 +0100

    Main + BuildDom

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 'use strict'
+
+
+const main = () => {
+
+  const buildDom = (html) =>{
+    const main = document.querySelector('main');
+    main.innerHTML = html;
+    return main;
+  };
+
+  
+
+
+
+
+}
+
+window.addEventListener('load', main);
